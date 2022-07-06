d554d8d632dca313979dc6d025bc2a72b30795b9
Gekkabijin <a_queen_of_the_night@outlook.com>
2022-07-07 04:31:57 +0900

    add inine keyword
    Synonyms for __inline and __forceinline

---
+++ MirageEngine/SourceFiles/System/Inline/Inline.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file  Inline.hpp
+ * @brief Inline keyword
+ */
+#pragma once
+
+
+/**
+ * @brief  inline keyword
+ * 
+ * @remark Not always deployed (the compiler's discretion)
+ */ 
+#define INLINE __inline
+
+/**
+ * @brief  forceinline keyword
+ * 
+ * @remark Not always deployed (the compiler's discretion)
+ */ 
+#define FORCEINLINE __forceinline
