78fe5f68ecab2e4df91aaf298c2d0de44861d535
Gekkabijin <a_queen_of_the_night@hotmail.co.jp>
2025-08-02 07:55:11 +0900

    refactor: インラインの定義名、インクルードガード、ファイルの文字コードを変更
    インラインの定義名が被る可能性があったため、エンジン用に明示的な命名に変更した。
    インクルードガードが被る可能性があったため、システム内の項目も追加するようにした。（項目：SYSTEMやUTILITYなどの単位）
    ドキュメント自動生成時に文字化けを防ぐため、文字コードをUTF-8に変更した。

--- include/MirageEngine/System/Inline/Inline.hpp
+++ include/MirageEngine/System/Inline/Inline.hpp
@@ -1,51 +1,51 @@
-/**
+﻿/**
  * @file Inline.hpp
  * @brief インライン定義
  */
-#ifndef MIRAGE_ENGINE_INLINE_HPP
-#define MIRAGE_ENGINE_INLINE_HPP
+#ifndef MIRAGE_ENGINE_SYSTEM_INLINE_HPP
+#define MIRAGE_ENGINE_SYSTEM_INLINE_HPP
 
 
-#ifdef _MSC_VER
+#ifdef _MSC_VER // Microsoftコンパイラ
     /**
      * @def INLINE
-     * @brief 関数をインライン化するように指示します。（Microsoftコンパイラ）
+     * @brief 関数をインライン化するように指示します。
      *
      * @attention インライン化されるかはコンパイラ次第
      */
-    #define INLINE __inline
+    #define MIRAGE_ENGINE_INLINE __inline
 
-#elif defined(__GNUC__) || defined(__clang__)
+#elif defined(__GNUC__) || defined(__clang__)   // gcc, clangコンパイラ
      /**
       * @def INLINE
-      * @brief 関数をインライン化するように指示します。（gcc, clangコンパイラ）
+      * @brief 関数をインライン化するように指示します。
       *
       * @attention インライン化されるかはコンパイラ次第
       */
-    #define INLINE inline
+    #define MIRAGE_ENGINE_INLINE inline
 
 #endif  // _MSC_VER
 
 
-#ifdef _MSC_VER
+#ifdef _MSC_VER // Microsoftコンパイラ
     /**
      * @def FORCEINLINE
-     * @brief 関数を強制的にインライン化するように指示します。（Microsoftコンパイラ）
+     * @brief 関数を強制的にインライン化するように指示します。
      *
      * @attention インライン化されるかはコンパイラ次第
      */
-    #define FORCEINLINE __forceinline
+    #define MIRAGE_ENGINE_FORCEINLINE __forceinline
 
-#elif defined(__GNUC__) || defined(__clang__)
+#elif defined(__GNUC__) || defined(__clang__)   // gcc, clangコンパイラ
     /**
      * @def FORCEINLINE
-     * @brief 関数を強制的にインライン化するように指示します。（gcc, clangコンパイラ）
+     * @brief 関数を強制的にインライン化するように指示します。
      *
      * @attention インライン化されるかはコンパイラ次第
      */
-    #define FORCEINLINE __attribute__((always_inline)) inline
+    #define MIRAGE_ENGINE_FORCEINLINE __attribute__((always_inline)) inline
 
 #endif  // _MSC_VER
 
 
-#endif  // MIRAGE_ENGINE_INLINE_HPP
+#endif  // MIRAGE_ENGINE_SYSTEM_INLINE_HPP
